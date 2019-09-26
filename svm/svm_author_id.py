be3aa06641403126cb43f265e06df590f96dd23d
Pedro Ivo <pi.cruz@outlook.com>
2019-09-26 17:25:41 -0300

    SVM Class and Quiz

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,5 +26,38 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+from sklearn.svm import SVC
 
+# The folowing two lines reduces the length of the training dataset used
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
+# Trainning
+print 'Training'
+clf = SVC(C = 10000, kernel="rbf")
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training time: ", round(time() - 3), "s"
+
+print 'Predicting'
+t0 = time()
+pred = clf.predict(features_test)
+print "Prediction time: ", round(time() - 3), "s"
+
+print 'Evaluating accuracy'
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+# Collecting results
+print pred[10]
+print pred[26]
+print pred[50]
+
+print labels_test[10]
+print labels_test[26]
+print labels_test[50]
+
+num_chris = sum(pred)
+print "Number of detected Chris' emails: ", num_chris
+
+print acc
